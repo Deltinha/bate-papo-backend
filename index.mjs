a5cf145110b476a2735483e75fd03bc4ea9e22b7
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-10-11 15:02:37 -0300

    Receive messages

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import cors from 'cors';
+import dayjs from 'dayjs';
 import express from 'express';
 
 const app = express();
@@ -6,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 const participants = [];
+const messages = [];
 
 app.post('/participants', (req, res) => {
     const username = req.body.name;
@@ -21,8 +23,17 @@ app.post('/participants', (req, res) => {
     }
 })
 
-app.get("/participants", (req, res) => { 
+app.get('/participants', (req, res) => { 
     res.send(participants);
+})
+
+app.post('/messages', (req, res) => {
+    const {to, text, type} = req.body;
+    const time = dayjs().format('HH:mm:ss');
+    const from = req.headers['user'];
+    messages.push({to, text, type, time, from})
+    console.log(messages)
+    res.sendStatus(200);
 })
 
 app.listen(4000);
